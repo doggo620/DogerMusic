2d0c62320274ab516012f60baa2d0f0b49206398
doggo620 <igorzlom9@gmail.com>
2025-04-14 16:25:54 +0200

    First working print

---
+++ DogerMusic/dUI.cpp
@@ -0,0 +1,10 @@
+#include "dUI.hpp"
+#include <vector>
+
+class dUI::UIManager {
+private:
+	std::vector<dUI::Button> buttons;
+	std::vector<dUI::Animable> animables;
+public:
+	
+};
---
+++ DogerMusic/dUI.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace dUI {
+	class UIManager;
+	class UIElement;
+	class Button;
+	class Animation;
+	class Animable;
+}
